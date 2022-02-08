6f75fefb276abfc29fc9f6dcffaadf405c39fa88
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-02-09 00:40:08 +0330

    footer styled 2

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,36 +6,46 @@ import { AiFillGithub } from 'react-icons/ai';
 
 const Footer = () => {
   return (
-    <footer className={style.footerTag}>
-      <section className={style.description}>
-        {' '}
-        inspaired by Saheb Mohammadi's React & Redux curse.{' '}
-      </section>
-      <ul className={style.ulTag}>
-        <li className={style.liTag}>
-          <a href="https://www.instagram.com/sina_gh1999">
-            <AiFillInstagram />
-          </a>{' '}
-        </li>
-        <li className={style.liTag}>
-          <a href="https://github.com/sinaGh-cyber">
-            <AiFillGithub />
-          </a>{' '}
-        </li>
-        <li className={style.liTag}>
+    <>
+      <footer className={style.footerTag}>
+        <section className={style.description}>
           {' '}
-          <a href="mailto:mohammad.sina.gholami.cyber@gmail.com">
-            <AiFillMail />
-          </a>{' '}
-        </li>
-        <li className={style.liTag}>
-          {' '}
-          <a href="https://www.linkedin.com/in/mohammad-sina-gholami-632489214/">
-            <AiOutlineLinkedin />
-          </a>{' '}
-        </li>
-      </ul>
-    </footer>
+          <p>
+            coded by <a href="https://github.com/sinaGh-cyber">me</a>
+          </p>
+          <br />
+          <p>
+            inspired by{' '}
+            <a href="https://github.com/sahebmohammadi">Saheb Mohammadi</a>'s
+            React & Redux curse.
+          </p>{' '}
+        </section>
+        <ul className={style.ulTag}>
+          <li className={style.liTag}>
+            <a href="https://www.instagram.com/sina_gh1999">
+              <AiFillInstagram />
+            </a>{' '}
+          </li>
+          <li className={style.liTag}>
+            <a href="https://github.com/sinaGh-cyber">
+              <AiFillGithub />
+            </a>{' '}
+          </li>
+          <li className={style.liTag}>
+            {' '}
+            <a href="mailto:mohammad.sina.gholami.cyber@gmail.com">
+              <AiFillMail />
+            </a>{' '}
+          </li>
+          <li className={style.liTag}>
+            {' '}
+            <a href="https://www.linkedin.com/in/mohammad-sina-gholami-632489214/">
+              <AiOutlineLinkedin />
+            </a>{' '}
+          </li>
+        </ul>
+      </footer>
+    </>
   );
 };
 
