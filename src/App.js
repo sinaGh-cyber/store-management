84f6133f624d338a4921ba4cc93eb1dd1cdcb5cb
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-02-08 03:53:31 +0330

    css replaced with scss2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import Navbar from './components/navbar/navbar';
 import Footer from './components/footer/footer';
 import Main from './components/main/main';
