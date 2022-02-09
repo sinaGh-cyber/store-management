c8636f928df0875540196d8577bd4eb8054ef7a4
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-02-10 01:27:41 +0330

    useThemeMode imported in Navbar

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,11 +1,22 @@
 import style from './navbar.module.scss';
 
+import { useThemeMode } from '../../context/themeModeProvider';
+import { posableThemeModes } from '../../context/themeModeProvider';
+
 import { FiRefreshCw } from 'react-icons/fi';
 import { AiFillBulb } from 'react-icons/ai';
 import { AiOutlineBulb } from 'react-icons/ai';
+import { useState } from 'react';
 
 
 const Navbar = () => {
+
+  const {themeMode, themeModeToggler} = useThemeMode();
+  const [isDark, setIsDark] = useState(false);
+
+  
+  if(themeMode === posableThemeModes.DARK) setIsDark(true);
+
   return (
     <nav className={style.navTag}>
       <header className={style.headerTag}></header>
