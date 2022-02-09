a9f55ef6fb714955f18065156bcc369be8435c87
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-02-09 17:10:55 +0330

    navbar main jsx added + toggle btn

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,7 +1,27 @@
 import style from './navbar.module.scss';
+import toggleThemeMode from './navbar.toggleThemeMode.module.scss';
 
 const Navbar = () => {
-    return ( <nav className={style.navTag} ></nav> );
-}
- 
+  return (
+    <nav className={style.navTag}>
+      <header className={style.headerTag}></header>
+
+      <section className={style.buttonGroup}>
+        <button className={style.NavbarRefreshBtn}></button>
+
+        <div className={style.toggleThemeMode}>
+          <div
+            className={`${toggleThemeMode.button} ${toggleThemeMode.r}`}
+            id={toggleThemeMode.button1}
+          >
+            <input type="checkbox" className={toggleThemeMode.checkbox} />
+            <div className={toggleThemeMode.knobs}></div>
+            <div className={toggleThemeMode.layer}></div>
+          </div>
+        </div>
+      </section>
+    </nav>
+  );
+};
+
 export default Navbar;
