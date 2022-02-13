3512742f96e96493fbcc59878a46f5f4c256a3d6
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-02-14 01:19:02 +0330

    ProductsLis's dipenencys imported and dark mode funtionalty added

--- src/components/productsList/productsList.jsx
+++ src/components/productsList/productsList.jsx
@@ -1,9 +1,35 @@
+import style from './productsList.module.scss';
 
+import { useThemeMode } from '../../context/themeModeProvider';
+import { posableThemeModes } from '../../context/themeModeProvider';
+
+import { useEffect, useState } from 'react';
+
+import { useProductsActions } from '../../context/productsProvider';
 
 const ProductsList = () => {
-    return ( <>
-    <section className="productsList"></section>
-    </> );
-}
- 
+  const { themeMode } = useThemeMode();
+  const [isDark, setIsDark] = useState(false);
+
+  const dispatch = useProductsActions();
+
+  useEffect(() => {
+    if (themeMode === posableThemeModes.DARK) {
+      setIsDark(true);
+    } else {
+      setIsDark(false);
+    }
+  }, [themeMode]);
+
+  return (
+    <>
+      <section
+        className={`${style.productsList} ${
+          isDark ? style.bgDark : style.bgLight
+        }`}
+      ></section>
+    </>
+  );
+};
+
 export default ProductsList;
