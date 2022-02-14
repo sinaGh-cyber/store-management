52e89bb97cce0f778313b897cffc37e1de5c9af1
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-02-14 04:37:29 +0330

    main slelton of returned jsx of product component added

--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -1,9 +1,26 @@
 import style from './product.module.scss';
 
-const Product = () => {
+import { useProductsActions } from '../../context/productsProvider';
 
-    
-    return ( <div className="product"></div> );
-}
- 
+const Product = ({ isDark, product }) => {
+  const dispatch = useProductsActions();
+
+  return (
+    <div
+      className={`${style.product} ${isDark ? style.bgDark : style.bgLight}`}
+    >
+      <div className={style.productInfo}>
+          <p className={style.productName} >Product name: {product.title} </p>
+          <p className={style.productPrice} >price: {product.price}</p>
+          <span className={style.productQuantity} >{product.quantity}</span>
+      </div>
+      <div className={style.productManagement}>
+          <button className={style.delete}></button>
+          <button className={style.increase}></button>
+          <button className={style.decrease}></button>
+      </div>
+    </div>
+  );
+};
+
 export default Product;
