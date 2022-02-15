d17ecdac276dc00a5d5880475e8453dbc9ad8b92
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-02-15 05:11:43 +0330

    product component ready: themeMode, Responsive, jsx

--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -1,8 +1,16 @@
 import style from './product.module.scss';
 
+import { AiOutlinePlus } from 'react-icons/ai';
+import { AiOutlineMinus } from 'react-icons/ai';
+import { AiFillDelete } from 'react-icons/ai';
+
 import { useProductsActions } from '../../context/productsProvider';
 
 const Product = ({ isDark, product }) => {
+  // conditional inline styling for decreaseBtn
+  const decreaseBtnStyle =
+    product.quantity > 1 && !isDark ? {} : { color: 'red' };
+
   const dispatch = useProductsActions();
 
   return (
@@ -10,14 +18,23 @@ const Product = ({ isDark, product }) => {
       className={`${style.product} ${isDark ? style.bgDark : style.bgLight}`}
     >
       <div className={style.productInfo}>
-          <p className={style.productName} >Product name: {product.title} </p>
-          <p className={style.productPrice} >price: {product.price}</p>
-          <span className={style.productQuantity} >{product.quantity}</span>
+        <p className={style.productName}>Product name: {product.title} </p>
+        <p className={style.productPrice}>price: {product.price}</p>
+        <span className={style.productQuantity}>{product.quantity}</span>
       </div>
       <div className={style.productManagement}>
-          <button className={style.delete}></button>
-          <button className={style.increase}></button>
-          <button className={style.decrease}></button>
+        <button
+          className={`${style.decrease} ${
+            product.quantity <= 1 ? style.toBeDeleted : null
+          }`}
+        >
+          {product.quantity > 1 ? <AiOutlineMinus /> : <AiFillDelete />}
+        </button>
+        <button className={style.increase}>
+          {' '}
+          <AiOutlinePlus />{' '}
+        </button>
+        <button className={style.delete}>Delete</button>
       </div>
     </div>
   );
