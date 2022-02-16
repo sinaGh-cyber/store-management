b3149f32bfe806db8800f7abc60598c65a8e603b
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-02-16 04:51:31 +0330

    prodouct provider cleaned up and delet and decrease actions added to reducer + try-ctch blocks added to reducer's cases

--- src/context/productsProvider.js
+++ src/context/productsProvider.js
@@ -5,40 +5,83 @@ import {
   useState,
   useEffect,
 } from 'react';
-import { getAllProductsAPI } from '../services/api/index';
+import {
+  getAllProductsAPI,
+  putProductApi,
+  deleteProductApi,
+} from '../services/api/index';
 
 const productsContext = createContext(undefined);
 const productsContextDispatcher = createContext(undefined);
 
-let availableProducts;
+let availableProducts = [];
 
 const reduce = async (stat, { type, id }) => {
   switch (type) {
     case 'refresh': {
-      let products = await getAllProductsAPI();
-      products = await products.data;
-      availableProducts = [...products];
-      return products;
+      let receivedProducts;
+      try {
+        receivedProducts = await getAllProductsAPI();
+        receivedProducts = await receivedProducts.data;
+        availableProducts = [...receivedProducts];
+      } catch (err) {
+        throw err;
+      } finally {
+        return receivedProducts;
+      }
+    }
+
+    case 'decrease': {
+      let res;
+      try {
+        availableProducts[id - 1].quantity--;
+        res = await putProductApi(id, availableProducts[id - 1]);
+      } catch (err) {
+        throw err;
+      } finally {
+        if (res.status < 300 && res.status > 199) {
+          return availableProducts;
+        } else {
+          availableProducts[id - 1].quantity++;
+          return stat;
+        }
+      }
+    }
+    case 'delete': {
+      try {
+        const res = await deleteProductApi(id);
+
+        if (res.status < 300 && res.status > 199) {
+          availableProducts = availableProducts.filter(
+            (product) => product.id !== id
+          );
+        }
+      } catch (err) {
+        throw err;
+      } finally {
+        return availableProducts;
+      }
     }
     default: {
       throw Error('there is no product in list');
     }
   }
 };
-const initData = 0;
 
 const ProductsProvider = ({ children }) => {
+  const initData = new Promise((resolve) => {
+    resolve([]);
+  });
   const [products, dispatch] = useReducer(reduce, initData);
-  const [extractedProductsArray, setExtractedProductsArray] = useState(false);
+  const [extractedProductsArray, setExtractedProductsArray] = useState([]);
 
   useEffect(() => {
-    products &&
-      products.then((data) => {
-        const Array = data.map((product) => {
-          return product;
-        });
-        setExtractedProductsArray(Array);
+    products.then((data) => {
+      const Array = data.map((product) => {
+        return product;
       });
+      setExtractedProductsArray(Array);
+    });
   }, [products]);
 
   return (
