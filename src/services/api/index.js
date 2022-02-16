bf2e309bf95cc239cffef8e01080e1f904621925
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-02-16 04:46:31 +0330

    delete and put api added

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -3,3 +3,8 @@ import Axios from 'axios';
 export const getAllProductsAPI = () =>
   Axios.get('http://127.0.0.1:8000/products/');
 
+export const putProductApi = (id, productObj) =>
+  Axios.put(`http://127.0.0.1:8000/products/${id}`, productObj);
+
+export const deleteProductApi = (id) =>
+  Axios.delete(`http://127.0.0.1:8000/products/${id}`);
